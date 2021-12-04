dde23124168f59d87d4ff298e49176fec79937ac
Dan Percic <danpercic86@gmail.com>
2021-12-04 12:57:44 +0200

    added project & models & views

---
+++ DotnetExample/DotnetExample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetExample.Models;
+using DotnetExample.Views;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotnetExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private static readonly WeatherForecastModel[] Database = GetModels();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecastResponseView> Get()
+        {
+            return Database.Select(weatherForecast => new WeatherForecastResponseView
+            {
+                Date = weatherForecast.Date,
+                TemperatureC = weatherForecast.TemperatureC,
+                Summary = weatherForecast.Summary
+            });
+        }
+
+        [HttpPost]
+        public string GetName()
+        {
+            return "My name";
+        }
+
+        private static WeatherForecastModel[] GetModels()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastModel
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+               .ToArray();
+        }
+    }
+}
---
+++ DotnetExample/DotnetExample/Views/WeatherForecastResponseView.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DotnetExample.Views
+{
+    public class WeatherForecastResponseView
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
